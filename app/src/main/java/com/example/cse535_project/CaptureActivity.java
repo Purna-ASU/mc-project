0fec6f891fcd041399422eadfcdf4cfca099cfcc
Prasidh Aggarwal <aprasidh@gmail.com>
2022-11-08 01:19:49 -0700

    working project2 on computer images

--- app/src/main/java/com/example/cse535_project/CaptureActivity.java
+++ app/src/main/java/com/example/cse535_project/CaptureActivity.java
@@ -1,7 +1,13 @@
 package com.example.cse535_project;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -25,13 +31,12 @@ import java.io.FileOutputStream;
 import cz.msebera.android.httpclient.Header;
 
 @SuppressWarnings("ALL")
-public class CaptureActivity extends AppCompatActivity {
+public class CaptureActivity extends PreProcessImage {
     // Add local IP address here in SERVER_URL
-    public static final String SERVER_URL = "http://172.20.10.7:80";
+    public static final String SERVER_URL = "http://192.168.8.112:80";
 
     ImageView imageView;
     String finalFileName;
-    String dropDownValue;
 
     public Uri imageCaptureUrl;
     int CAMERA_PIC_REQUEST = 102;
@@ -52,6 +57,7 @@ public class CaptureActivity extends AppCompatActivity {
         if (requestCode == CAMERA_PIC_REQUEST) {
 
             Bitmap photo = (Bitmap) data.getExtras().get("data");
+            Bitmap finalImage = PreProcessImage.greyscale(photo);
             imageView = (ImageView) findViewById(R.id.imageView);
             imageView.setImageBitmap(photo);
             Log.i("CAPTURE", String.valueOf(imageCaptureUrl));
@@ -59,14 +65,13 @@ public class CaptureActivity extends AppCompatActivity {
             File myDir = new File(root);
             myDir.mkdirs();
             String fileName = System.currentTimeMillis
-                    () + "_image.png";
+                    () + "_image.jpeg";
             File file = new File(myDir, fileName);
             Log.i("CAPTURE", root + fileName);
-
             finalFileName = root + fileName;
             try {
                 FileOutputStream out = new FileOutputStream(file);
-                photo.compress(Bitmap.CompressFormat.PNG, 90, out);
+                photo.compress(Bitmap.CompressFormat.JPEG, 100, out);
                 out.flush();
                 out.close();
             } catch (Exception e) {
@@ -75,29 +80,23 @@ public class CaptureActivity extends AppCompatActivity {
         }
     }
 
-    public void selectCategory(View view) {
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        dropDownValue = spinner.getSelectedItem().toString();
-        if (!dropDownValue.contains("Choose a category")) {
-            uploadToServer();
-            Intent intent = new Intent(CaptureActivity.this, MainActivity.class);
-            startActivity(intent);
-        } else {
-            Toast.makeText(CaptureActivity.this, "Choose a value", Toast.LENGTH_SHORT).show();
-        }
+    public void detectDigit(View view) {
+        DigitsDetector digitsDetector = new DigitsDetector(this.getApplicationContext());
+        Bitmap bm=((BitmapDrawable)imageView.getDrawable()).getBitmap();
+        int digit = digitsDetector.detectDigit(bm);
+        Log.i("ANSWER", String.valueOf(digit));
+        uploadToServer(digit);
+        Intent intent = new Intent(CaptureActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 
     // Method to upload file to server
-    public void uploadToServer() {
-
+    public void uploadToServer(int digit) {
         String finalUploadUrl = SERVER_URL + "/uploadFile";
         AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
         File file = new File(finalFileName);
-        Log.i("CAPTURE", "Delta path new - " + finalFileName);
-
         RequestParams fileUploadParameters = new RequestParams();
-
-        fileUploadParameters.put("categoryvalue", dropDownValue);
+        fileUploadParameters.put("categoryvalue", digit);
         try {
             fileUploadParameters.put("choosefile", file);
         } catch (FileNotFoundException e) {
@@ -108,7 +107,7 @@ public class CaptureActivity extends AppCompatActivity {
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                Toast toast = Toast.makeText(getApplicationContext(), "Successful upload!", Toast.LENGTH_SHORT);
+                Toast toast = Toast.makeText(getApplicationContext(), "Successfully uploaded" + "  "+ digit, Toast.LENGTH_SHORT);
                 toast.show();
             }
 
@@ -119,4 +118,5 @@ public class CaptureActivity extends AppCompatActivity {
             }
         });
     }
+
 }
